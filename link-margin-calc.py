df263467dd20c17b86a2f6f5ad5977602d1cb8d2
Adam Kimbrough <adamk@users.noreply.github.com>
2020-06-02 15:07:37 -0400

    Update link-margin-calc.py
    Added functions

--- link-margin-calc.py
+++ link-margin-calc.py
@@ -1,7 +1,6 @@
 # Adam Kimbrough - Link Margin Calculator
 from math import log10, pow
-
-import math
+import matplotlib.pyplot as plt
 
 # Gains
 tx_pwr = 0;
@@ -41,87 +40,94 @@ rx_feed = float(input('Enter RX feed loss: '));
 hb = tx_ant_height;
 hm = rx_ant_height;
 
-have_rx_sens = input("Enter 'Y' if you have RX sensitivity, otherwise, 'N'...  ")
-if (have_rx_sens.upper() == 'Y'):
-	manual_margin = int(input('Enter RX sensitivity: '));
+def thermal_noise_power(tnd, bw):
+	tnp = tnd + 10*log10(1000000*bw);
+	return tnp
 
-	prop_modelap = input("Choose propagation model: 'FSPL', 'HMS'(Hata med-small), 'HL'(Hata large), 'FSPL2'(ground refl only): ")
+def pl_calc(txpwr,txant, rxant,txant_height, rxant_height, dist, freq, bw, pwrbackoff, txfeed, rxfeed):
+	have_rx_sens = input("Enter 'Y' if you have RX sensitivity, otherwise, 'N'...  ")
+	if (have_rx_sens.upper() == 'Y'):
+		manual_margin = int(input('Enter RX sensitivity: '));
 
-	if (prop_model.upper() == 'FSPL'):
-		path_loss = 20*log10(distance) + 20*log10(freq) + 32.44
+		prop_model = input("Choose propagation model: 'FSPL', 'HMS'(Hata med-small), 'HL'(Hata large), 'FSPL2'(ground refl only): ")
 
-	elif (prop_model.upper() == 'HMS'):
+		if (prop_model.upper() == 'FSPL'):
+			path_loss = 20*log10(distance) + 20*log10(freq) + 32.44
 
-		# Correction factor (medium-small city)
-		a_hm = (1.1*log10(freq)-0.7)*hm - (1.56*log10(freq)-0.8)
+		elif (prop_model.upper() == 'HMS'):
 
-		# General Hata Model Equation
-		Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
-		# Hata Model (Open Area) - based on urban losses
-		path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
-	elif (prop_model == 'HL'):
-		# Correction factor (large city)
-		a_hm2 = 3.2*(pow(log10(11.75*hm),2)) - 4.97
-		# General Hata Model Equation
-		Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
-		# Hata Model (Open Area) - based on urban losses
-		path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
-	elif (prop_model.upper() == 'FSPL2'):
-		# Free Space Path Loss Model w/ Ground Reflection
-		G = tx_ant * rx_ant
-		path_loss = 40*log10(distance*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2))
+			# Correction factor (medium-small city)
+			a_hm = (1.1*log10(freq)-0.7)*hm - (1.56*log10(freq)-0.8)
+
+			# General Hata Model Equation
+			Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
+			# Hata Model (Open Area) - based on urban losses
+			path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
+		elif (prop_model == 'HL'):
+			# Correction factor (large city)
+			a_hm2 = 3.2*(pow(log10(11.75*hm),2)) - 4.97
+			# General Hata Model Equation
+			Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
+			# Hata Model (Open Area) - based on urban losses
+			path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
+		elif (prop_model.upper() == 'FSPL2'):
+			# Free Space Path Loss Model w/ Ground Reflection
+			G = tx_ant * rx_ant
+			path_loss = 40*log10(distance*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2))
+		else:
+			print('Invalid option')
+
+		power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
+
+		print('--------------------------------');
+		print('Path loss: ', path_loss);
+		print('Power left at input: ', power_at_rx);
+		print('**Link Margin** ');
+		print(power_at_rx - manual_margin)
+
+	elif (have_rx_sens.upper() == 'N'):
+		noise_figure = float(input('Enter receiver NF: '));
+		sig_to_noise = float(input('Enter receiver\'s required S/N: '));
+
+		prop_model = input("Choose propagation model: 'FSPL', 'HMS'(Hata med-small), 'HL'(Hata large), 'FSPL2'(ground refl only): ")
+		if (prop_model.upper() == 'FSPL'):
+			path_loss = 20*log10(distance) + 20*log10(freq) + 32.44
+
+		elif (prop_model.upper() == 'HMS'):
+
+			# Correction factor (medium-small city)
+			a_hm = (1.1*log10(freq)-0.7)*hm - (1.56*log10(freq)-0.8)
+
+			# General Hata Model Equation
+			Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
+			# Hata Model (Open Area) - based on urban losses
+			path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
+		elif (prop_model.upper() == 'HL'):
+			# Correction factor (large city)
+			a_hm2 = 3.2*(pow(log10(11.75*hm),2)) - 4.97
+			# General Hata Model Equation
+			Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
+			# Hata Model (Open Area) - based on urban losses
+			path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
+		elif (prop_model.upper() == 'FSPL2'):
+			# Free Space Path Loss Model w/ Ground Reflection
+			G = tx_ant * rx_ant
+			path_loss = 40*log10(distance*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2))
+		else:
+			print('Invalid option');
+
+		thermal_noise_pwr = thermal_noise_power(thermal_noise_density, detect_bw)
+		power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
+		rx_sensitivity = thermal_noise_pwr + noise_figure + sig_to_noise;
+		link_margin = power_at_rx - rx_sensitivity;
+		print('--------------------------------');
+		print('Path loss: ', path_loss);
+		print('Power left at input: ', power_at_rx);
+		print('Receiver sensitivity: ', rx_sensitivity);
+		print('**Link Margin** ');
+		print(link_margin)
+
 	else:
-		print('Invalid option')
+		print('Invalid input');
 
-	power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
-
-	print('--------------------------------');
-	print('Path loss: ', path_loss);
-	print('Power left at input: ', power_at_rx);
-	print('**Link Margin** ');
-	print(power_at_rx - manual_margin)
-
-elif (have_rx_sens.upper() == 'N'):
-	noise_figure = float(input('Enter receiver NF: '));
-	sig_to_noise = float(input('Enter receiver\'s required S/N: '));
-
-	prop_model = input("Choose propagation model: 'FSPL', 'HMS'(Hata med-small), 'HL'(Hata large), 'FSPL2'(ground refl only): ")
-	if (prop_model.upper() == 'FSPL'):
-		path_loss = 20*log10(distance) + 20*log10(freq) + 32.44
-
-	elif (prop_model.upper() == 'HMS'):
-
-		# Correction factor (medium-small city)
-		a_hm = (1.1*log10(freq)-0.7)*hm - (1.56*log10(freq)-0.8)
-
-		# General Hata Model Equation
-		Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
-		# Hata Model (Open Area) - based on urban losses
-		path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
-	elif (prop_model.upper() == 'HL'):
-		# Correction factor (large city)
-		a_hm2 = 3.2*(pow(log10(11.75*hm),2)) - 4.97
-		# General Hata Model Equation
-		Lp = 69.55 + 26.16*log10(freq) - 13.82*log10(hb) - a_hm + (44.9 - 6.55*log10(hb))*log10(distance);
-		# Hata Model (Open Area) - based on urban losses
-		path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
-	elif (prop_model.upper() == 'FSPL2'):
-		# Free Space Path Loss Model w/ Ground Reflection
-		G = tx_ant * rx_ant
-		path_loss = 40*log10(distance*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2))
-	else:
-		print('Invalid option');
-
-	thermal_noise_pwr = thermal_noise_density + 10*log10(1000000*detect_bw);
-	power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
-	rx_sensitivity = thermal_noise_pwr + noise_figure + sig_to_noise;
-	link_margin = power_at_rx - rx_sensitivity;
-	print('--------------------------------');
-	print('Path loss: ', path_loss);
-	print('Power left at input: ', power_at_rx);
-	print('Receiver sensitivity: ', rx_sensitivity);
-	print('**Link Margin** ');
-	print(link_margin)
-
-else:
-	print('Invalid input');
+pl_calc(tx_pwr,tx_ant, rx_ant, tx_ant_height, rx_ant_height, distance, freq, detect_bw, pwr_back_off, tx_feed, rx_feed)
