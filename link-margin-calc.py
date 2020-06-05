951616c509ed5aea4320db88e3a4f0e1292789e5
Adam Kimbrough <adamk@users.noreply.github.com>
2020-06-05 07:56:52 -0400

    Update link-margin-calc.py

--- link-margin-calc.py
+++ link-margin-calc.py
@@ -1,5 +1,5 @@
 # Adam Kimbrough - Link Margin Calculator
-from math import log10, pow
+from math import log10, pow, pi, atan, sqrt, sin, cos
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -49,6 +49,9 @@ rx_feed = float(input('Enter RX feed loss: '));
 
 hb = tx_ant_height;
 hm = rx_ant_height;
+
+def sign(a):
+    return (a > 0) - (a < 0)
 
 def thermal_noise_power(tnd, bw):
 	tnp = tnd + 10*log10(1000000*bw);
@@ -107,6 +110,7 @@ def pl_calc():
 		if (prop_model.upper() == 'FSPL'):
 			path_loss = 20*log10(distance) + 20*log10(freq) + 32.44
 			fspl_bool = True
+			power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
 		elif (prop_model.upper() == 'HMS'):
 
 			# Correction factor (medium-small city)
@@ -117,6 +121,8 @@ def pl_calc():
 			# Hata Model (Open Area) - based on urban losses
 			path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
 			hms_bool = True
+			power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
+
 		elif (prop_model.upper() == 'HL'):
 			# Correction factor (large city)
 			a_hm2 = 3.2*(pow(log10(11.75*hm),2)) - 4.97
@@ -125,16 +131,37 @@ def pl_calc():
 			# Hata Model (Open Area) - based on urban losses
 			path_loss = Lp - 4.78*(pow(log10(freq), 2)) + 18.33*log10(freq) - 40.94;
 			hl_bool = True
+			power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
+
 		elif (prop_model.upper() == 'FSPL2'):
 			# Free Space Path Loss Model w/ Ground Reflection
-			G = tx_ant * rx_ant
-			path_loss = 40*log10(distance*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2))
+			# G = tx_ant * rx_ant
+			# path_loss = 40*log10(distance*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2))
 			fspl2_bool = True
+
+			c = 299.972458 * pow(10,6); # Speed of light in vaccum [m/s]
+			Gr = rx_ant; # Antenna Gain receiving antenna.
+			Gt = tx_ant; # Antenna Gain transmitting antenna.
+			Pt = pow(10, tx_pwr/10)/1000; # Energy to the transmitting antenna [Watt]
+			er = 18;
+			wave_len = c/(freq*1000000)
+			phi = atan((hb+hm)/(distance*1000)); # phi incident angle to ground.
+			direct_wave=sqrt(pow(abs(hb-hm),2)+pow(distance*1000, 2)); # Distance, traveled direct wave
+			refl_wave=sqrt(pow(distance*1000,2)+pow((hb+hm),2)); # Distance, traveled reflected wave
+			gamma=(er*sin(phi)-sqrt(er-pow(cos(phi),2)))/(er*sin(phi)+sqrt(er-pow(cos(phi),2))); # Vertical polarization
+			length_diff=refl_wave-direct_wave;
+			cos_phase_diff=cos(length_diff*2*pi/wave_len)*sign(gamma);
+			Direct_energy=Pt*Gt*Gr*pow(wave_len,2)/(pow((4*pi*direct_wave),2));
+			reflected_energy=Pt*Gt*Gr*pow(wave_len,2)/(pow((4*pi*refl_wave),2))*abs(gamma);
+			Total_received_energy=Direct_energy+cos_phase_diff*reflected_energy;
+			Total_received_energy_dBm=10*log10(Total_received_energy*1e3);
+			path_loss = (tx_pwr+tx_ant+rx_ant) - Total_received_energy_dBm - (pwr_back_off + tx_feed + rx_feed);
+			power_at_rx = Total_received_energy_dBm - (pwr_back_off + tx_feed + rx_feed);
+
 		else:
 			print('Invalid option');
 
 		thermal_noise_pwr = thermal_noise_power(thermal_noise_density, detect_bw)
-		power_at_rx = (tx_pwr+tx_ant+rx_ant) - path_loss - (pwr_back_off + tx_feed + rx_feed);
 		rx_sensitivity = thermal_noise_pwr + noise_figure + sig_to_noise;
 		link_margin = power_at_rx - rx_sensitivity;
 		print('--------------------------------');
@@ -147,10 +174,16 @@ def pl_calc():
 	else:
 		print('Invalid input');
 
-	return (fspl_bool, fspl2_bool, hms_bool, hl_bool)
+	return (fspl_bool, fspl2_bool, hms_bool, hl_bool, tx_pwr, tx_ant, rx_ant)
 
 
-def plp(pl, total_dist, model1, model2, model3, model4, tx_freq, tnp, nf, sn):
+def plp(pl, total_dist, model1, model2, model3, model4, tx_freq, tnp, nf, sn, txp, txant, rxant):
+	distance2 = float(input('Enter assumed max FSPL distance (mi) from table: '));
+	path_loss2 = 20*log10(distance2*1.60934) + 20*log10(freq) + 32.44
+	power_at_rx2 = (txp+txant+rxant) - path_loss2; # neglects (pwr_back_off + tx_feed + rx_feed)
+	rx_sensitivity2 = tnp + nf + sn;
+	link_margin2 = power_at_rx2 - rx_sensitivity2;
+	print(link_margin2)
 	path_loss_arr = []
 	link_margin_arr = []
 	dist_arr = []
@@ -166,11 +199,26 @@ def plp(pl, total_dist, model1, model2, model3, model4, tx_freq, tnp, nf, sn):
 			i += 1
 
 		if model2:
-			G = tx_ant * rx_ant
-			path_loss_arr.append(40*log10(dist*1000) - 10*log10(G*pow(hb,2)*pow(hm, 2)))
-			dist_arr.append(dist)
-			power_at_rx = float((tx_pwr+tx_ant+rx_ant) - path_loss_arr[i] - (pwr_back_off + tx_feed + rx_feed));
+			c = 299.972458 * pow(10,6); # Speed of light in vaccum [m/s]
+			Gr = rx_ant; # Antenna Gain receiving antenna.
+			Gt = tx_ant; # Antenna Gain transmitting antenna.
+			Pt = pow(10, tx_pwr/10)/1000; # Energy to the transmitting antenna [Watt]
+			er = 18;
+			wave_len = c/(freq*1000000)
+			phi = atan((hb+hm)/(dist*1000)); # phi incident angle to ground.
+			direct_wave=sqrt(pow(abs(hb-hm),2)+pow(dist*1000, 2)); # Distance, traveled direct wave
+			refl_wave=sqrt(pow(distance*1000,2)+pow((hb+hm),2)); # Distance, traveled reflected wave
+			gamma=(er*sin(phi)-sqrt(er-pow(cos(phi),2)))/(er*sin(phi)+sqrt(er-pow(cos(phi),2))); # Vertical polarization
+			length_diff=refl_wave-direct_wave;
+			cos_phase_diff=cos(length_diff*2*pi/wave_len)*sign(gamma);
+			Direct_energy=Pt*Gt*Gr*pow(wave_len,2)/(pow((4*pi*direct_wave),2));
+			reflected_energy=Pt*Gt*Gr*pow(wave_len,2)/(pow((4*pi*refl_wave),2))*abs(gamma);
+			Total_received_energy=Direct_energy+cos_phase_diff*reflected_energy;
+			Total_received_energy_dBm=10*log10(Total_received_energy*1e3);
+			path_loss_arr.append((tx_pwr+tx_ant+rx_ant) - Total_received_energy_dBm - (pwr_back_off + tx_feed + rx_feed));
+			power_at_rx = Total_received_energy_dBm - (pwr_back_off + tx_feed + rx_feed);
 			rx_sensitivity = float(tnp + nf + sn);
+			dist_arr.append(dist)
 			link_margin_arr.append(power_at_rx - rx_sensitivity)
 			i += 1
 
@@ -207,7 +255,7 @@ def plp(pl, total_dist, model1, model2, model3, model4, tx_freq, tnp, nf, sn):
 	plt.plot(x1, y2, "-g", label = "link margin")
 	j = 0
 	for xy in zip(x1,y1):
-		if y2[j] > 0.01 and y2[j] < 0.1:
+		if round(y2[j],2) == round(link_margin2, 2):
 			plt.annotate('Breakpoint: (%.2f, %.2f)' % xy, xy = xy, textcoords = 'data')
 			break
 		j += 1
@@ -224,6 +272,5 @@ def plp(pl, total_dist, model1, model2, model3, model4, tx_freq, tnp, nf, sn):
 	plt.legend(loc="center right")
 	plt.show()
 
-
-fspl_bool, fspl2_bool, hms_bool, hl_bool = pl_calc()
-plp(path_loss, distance, fspl_bool, fspl2_bool, hms_bool, hl_bool, freq, thermal_noise_pwr, noise_figure, sig_to_noise)
+fspl_bool, fspl2_bool, hms_bool, hl_bool, tx_pwr, tx_ant, rx_ant = pl_calc()
+plp(path_loss, distance, fspl_bool, fspl2_bool, hms_bool, hl_bool, freq, thermal_noise_pwr, noise_figure, sig_to_noise, tx_pwr, tx_ant, rx_ant)
